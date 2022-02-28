6f98f4cc23d4ebfc5ebfe68af486bb734858e969
KxngBari <49103177+KxngBari@users.noreply.github.com>
2022-02-28 18:49:15 +0000

    Creating list for events
    Creating a list that will store the events created in Koreography. Also making a gameplay controller that will control the properties of the gameplay

--- GTL1/Assets/Scripts/ButtonController.cs
+++ GTL1/Assets/Scripts/ButtonController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SonicBloom.Koreo;
 
-public class ButtonController : MonoBehaviour
+namespace GTL
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ButtonController : MonoBehaviour
     {
-        
-    }
+        public Color colorOfNotes = Color.blue;
+        public SpriteRenderer targetColour;
+        public KeyCode keyboardButton;
+        public List<string> payload = new List<string>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        List<KoreographyEvent> arrowEvents = new List<KoreographyEvent>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            targetColour.color = colorOfNotes;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
---
+++ GTL1/Assets/Scripts/GTLController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+namespace GTL
+{
+    
+    public class GTLController : MonoBehaviour
+    {
+        [EventID]
+        public string eventID;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
